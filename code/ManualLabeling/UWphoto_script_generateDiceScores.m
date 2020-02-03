82085dc2cabb8f8bbe420545ea7ce6d1efa418dc
Henry Tregidgo <h.tregidgo@ucl.ac.uk>
2020-02-03 10:00:43 +0000

    Adds script structure for dice calc - Issue #7
    Adds the script UWphoto_script_generateDiceScores.m with outline of
    script structure placed as section headings and comments.

---
+++ code/ManualLabeling/UWphoto_script_generateDiceScores.m
@@ -0,0 +1,19 @@
+%% find segmentation volumes
+% - manual label volumes
+% - matching samseg volumes
+
+
+%% merge labels 
+% - ventral DC & WM
+% - WM hypo-intensities
+% - choroid plexus and ventricles (or background)
+
+%% generate Dice by species 
+
+
+%% output stats to files 
+% - in figures
+
+%% output merged labels to files
+% - in relevant results folders
+
